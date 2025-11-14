b964e75022a9bb8cbad24a69794cfb28169e9943
이지은 <tori@ryanlab.kr>
2025-11-14 13:52:54 +0900

    header 스타일 작성

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,18 +15,37 @@ const Header = () => {
             <div className="container">
                 <h1 className="logo">
                     <a href="#">
-                        <img src="" alt="이지은 포트폴리오 로고" />
+                        <img
+                            src="/portfolio-v2/img/header_logo.png"
+                            alt="이지은 포트폴리오 로고"
+                        />
                     </a>
                 </h1>
-                <button
-                    className={isMenuActive ? "menu_btn active" : "menu_btn"}
-                    onClick={handleMenuClick}
-                >
-                    <span className="sr_only">메뉴 버튼</span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </button>
+                <div className="github_blog_menu_wrap">
+                    <a
+                        className="github"
+                        href="#"
+                        target="_blank"
+                        title="이지은 깃허브"
+                    ></a>
+                    <a
+                        className="blog"
+                        href="#"
+                        target="_blank"
+                        title="이지은 블로그"
+                    ></a>
+                    <button
+                        className={
+                            isMenuActive ? "menu_btn active" : "menu_btn"
+                        }
+                        onClick={handleMenuClick}
+                    >
+                        <span className="sr_only">메뉴 버튼</span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
+                </div>
                 <nav className={isNavActive ? "active" : ""}>
                     <h2 className="sr_only">메인 네비게이션</h2>
                     <ul>
